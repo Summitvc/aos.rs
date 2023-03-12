0f5d2fbb7cb4a2f2e619624af713aabd91a75709
Summitvc <66954045+Summitvc@users.noreply.github.com>
2023-03-12 16:05:03 +0100

    thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use std::ffi::CString;
+mod utils;
+use std::ptr::{null, null_mut};
+
+use enet_sys::{self, enet_initialize};
+use enet_sys::*;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe{
+        enet_initialize();
+
+        let raw_address = utils::ip("aos://16777343:32887").unwrap();
+        let ip_addr = CString::new(raw_address[0].as_str()).unwrap().into_raw();
+        println!("{:?}", raw_address[0].as_str());
+        
+        let mut address = ENetAddress{
+            host: 0,
+            port: 32887
+        };
+        let mut event = ENetEvent{
+            type_: 0,
+            peer: null_mut(),
+            channelID: 0,
+            data: 0,
+            packet: null_mut(),
+        };
+        let client = enet_host_create (
+            null(),  // address to bind the server host to
+            32,        // allow up to 32 clients and/or outgoing connections
+            1,         // allow up to 2 channels to be used, 0 and 1
+            0,         // assume any amount of incoming bandwidth
+            0);        // assume any amount of outgoing bandwidth
+
+        if client.is_null() {
+            panic!("error: client create returned null")
+        }
+        println!("...ENet client host created");
+
+        enet_host_compress_with_range_coder(client);
+
+
+
+        enet_address_set_host(&mut address, CString::from_raw(ip_addr).as_ptr());
+
+        println!("{:?}, {:?}", &address.host, &address.port);
+
+        let peer = enet_host_connect(client, &address, 1, 3);
+        if peer.is_null() {
+            panic!("Failed to connect to server");
+        }
+
+
+        if enet_host_service(client, &mut event, 5000)  > 0 && 
+        event.type_ == _ENetEventType_ENET_EVENT_TYPE_CONNECT{
+            println!("Connection succeeded, {:?}", event.packet);
+        }
+        else{
+            enet_peer_reset(peer);
+        }
+    }
 }
+/*
+    TODO: FIX UTILS. MAKE ENetAddress accept from ip function
+
+ */
---
+++ src/utils.rs
@@ -0,0 +1,26 @@
+use std::fmt::Error;
+
+pub fn ip(address: &str) -> Result<Vec<String>, Error> {
+    if !address.starts_with("aos://") {
+        return Ok(address.split(":").map(String::from).collect());
+    }
+
+    let address = &address["aos://".len()..];
+    let mut addr_split = address.split(":").map(String::from).collect::<Vec<_>>();
+
+    if addr_split[0].split(".").nth(1).is_some() {
+        return Ok(addr_split);
+    }
+
+    let ip: u32 = match addr_split[0].parse() {
+        Ok(ip) => ip,
+        Err(_) => {
+            println!("Wrong input");
+            return Err(Error);
+        }
+    };
+
+    addr_split[0] = format!("{}.{}.{}.{}", ip & 255, (ip >> 8) & 255, (ip >> 16) & 255, (ip >> 24) & 255);
+
+    Ok(addr_split)
+}
